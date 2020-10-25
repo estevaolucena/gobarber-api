e4f18192b4058a552c9bf205eed52a395e0ce4f2
Estevao Lucena <estevao.lucena@somoseducacao.com.br>
2020-10-25 18:16:51 -0300

    test(forgot password): create a unit tests following TDD to forgot password function

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,44 @@
+/* eslint-disable prettier/prettier */
+import 'reflect-metadata'
+import { injectable, inject } from 'tsyringe'
+
+// import AppError from '@shared/errors/AppError'
+
+// import User from '@modules/users/infra/typeorm/entities/User'
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider'
+import AppError from '@shared/errors/AppError'
+import IUserRepository from '../repositories/IUsersRepository'
+import IUserTokensRepository from '../repositories/IUserTokensRepository'
+
+interface IRequest {
+  email: string
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUserRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('UserTokenRepository')
+    private userTokenRepository: IUserTokensRepository,
+
+  ) { }
+
+  public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new AppError('User does not exists.')
+    }
+
+    await this.userTokenRepository.generate(user.id)
+
+    this.mailProvider.sendMail(email, 'Pedido de recuperação de senha recebido.')
+  }
+}
+
+export default SendForgotPasswordEmailService
